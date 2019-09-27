bb495cfa1d2c76c8ae7597f48d0580f9b2d37177
Gladys Daniel <danygee900@yahoo.com>
2019-09-27 04:30:38 -0400

    third commit: added project description to comments

--- Cpp_Template_Library_Ex2_Set/src/ex2.cpp
+++ Cpp_Template_Library_Ex2_Set/src/ex2.cpp
@@ -3,7 +3,7 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : C++ Program to Implement Set in Stl
 //============================================================================
 
 #include <iostream>
